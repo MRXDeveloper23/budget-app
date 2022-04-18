77420f3f64ef65a6de68a01c7793371aef82871c
Khurmatillo <72973599+MRXDeveloper23@users.noreply.github.com>
2022-04-19 01:26:10 +0500

    Unit testing performed on Authorization service

--- client/budget-app/src/app/pages/services/auth.service.spec.ts
+++ client/budget-app/src/app/pages/services/auth.service.spec.ts
@@ -1,16 +1,81 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing'
+import {
+    HttpTestingController,
+    HttpClientTestingModule,
+} from '@angular/common/http/testing'
+import { RouterTestingModule } from '@angular/router/testing'
+import { AuthService } from './auth.service'
 
-import { AuthService } from './auth.service';
+describe('AuthService Test', () => {
+    const loginUrl = 'http://localhost:3000/login'
+    let service: AuthService
+    let httpController: HttpTestingController
 
-describe('AuthService', () => {
-  let service: AuthService;
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule, RouterTestingModule],
+        })
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthService);
-  });
+        service = TestBed.inject(AuthService)
+        httpController = TestBed.inject(HttpTestingController)
+    })
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+    it('Authorization service should be created', () => {
+        expect(service).toBeTruthy()
+    })
+
+    describe('Login', () => {
+        it('should return access token refresh token and expires in', (done: DoneFn) => {
+            const mockResponse = {
+                accessToken: 'accessToken',
+                expiresIn: 10000,
+                refreshToken: 'refreshToken',
+            }
+            service.login('ak@mk.com', '12345').subscribe({
+                next: (res) => {
+                    expect(res).toEqual(mockResponse)
+                    done()
+                },
+            })
+            const req = httpController.expectOne({
+                method: 'POST',
+                url: loginUrl,
+            })
+            req.flush(mockResponse)
+        })
+        it('setSession has to been called on success', (done: DoneFn) => {
+            spyOn(service as any, 'setSession')
+            const expectedResult = {
+                accessToken: 'accessToken',
+                expiresIn: 10000,
+                refreshToken: 'refreshToken',
+            }
+            service.login('ak@mk.com', '12345').subscribe(() => {
+                expect((service as any).setSession).toHaveBeenCalledOnceWith(
+                    expectedResult
+                )
+                done()
+            })
+            const req = httpController.expectOne({
+                method: 'POST',
+                url: loginUrl,
+            })
+            req.flush(expectedResult)
+        })
+        it('setSession has not been called on error', (done: DoneFn) => {
+            spyOn(service as any, 'setSession')
+            service.login('jbdbf@gmail.com', 'bubyvyv').subscribe({
+                error: (e) => {
+                    expect((service as any).setSession).not.toHaveBeenCalled()
+                    done()
+                },
+            })
+
+            const req = httpController.expectOne({
+                method: 'POST',
+                url: loginUrl,
+            })
+            req.error(new ProgressEvent('401'))
+        })
+    })
+})
